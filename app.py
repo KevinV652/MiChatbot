d7d515e73baf03b05b5f5e42ffa0bc9d12e8add4
Kevin Valenzuela <kvalenzuela652@unab.edu.co>
2024-11-10 01:03:13 -0500

    3er intento por DIOS

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import chatbot  # Importa tu lógica desde chatbot.py
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "¡Chatbot desplegado con Render!"
+
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    data = request.get_json()
+    user_message = data.get('message')
+    bot_response = chatbot.respuesta(user_message)
+    return jsonify({'response': bot_response})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
